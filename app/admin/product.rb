b5eec9a20a71cd1f762594c486803dbc6483457e
raj-odc <ksraj_odc@yahoo.com>
2016-10-09 15:43:57 +0530

    completed the looping of product

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,17 +1,13 @@
 ActiveAdmin.register Product do
-  permit_params :category_id,:price, :name, :sku, :description, :is_active, :image
+  permit_params :price, :name, :sku, :description, :is_active, :brand_id, :category_id, :image
   form do |f|
     f.inputs "Product Details" do
       f.input :name
       f.input :sku
       f.input :price
       f.input :description
-      f.fields_for :category do |ff|
-        ff.input :id, :label => 'Category', :as => :select, :collection => Category.all.collect {|p| [ p.name, p.id ] }, :selected => ( f.object.category ? f.object.category.id : nil), :include_blank => false
-      end
-      f.fields_for :brand do |ff|
-        ff.input :id, :label => 'Brand', :as => :select, :collection => Brand.all.collect {|p| [ p.name, p.id ] }, :selected => ( f.object.brand ? f.object.brand.id : nil), :include_blank => false
-      end
+      f.input :category, :label => 'Category', :as => :select, :collection => Category.all.collect {|p| [ p.name, p.id ] }, :selected => ( f.object.category ? f.object.category.id : nil), :include_blank => false
+      f.input :brand, :label => 'Brand', :as => :select, :collection => Brand.all.collect {|p| [ p.name, p.id ] }, :selected => ( f.object.brand ? f.object.brand.id : nil), :include_blank => false
       f.input :is_active
       f.input :image
       f.actions
